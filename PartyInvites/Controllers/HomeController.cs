c78a3af39aacddc98c2ae190e9aa78d9d6feb2f8
travisredd <travisredd@mail.weber.edu>
2019-05-11 21:50:00 -0600

    ....

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace PartyInvites.Controllers
             return View("MyView");
         }
 
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
